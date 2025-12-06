6464f4fe7be1a975e1fbea358d5f197877c99000
Daniel Gillis <daniel.gillis@snowflake.com>
2025-12-06 12:58:02 -0500

    minor updates for clarity

--- tasks/snow-cli/sql/batch-2/006-custom_tools.sql
+++ tasks/snow-cli/sql/batch-2/006-custom_tools.sql
@@ -1,47 +1,46 @@
-CREATE OR REPLACE FUNCTION INS_CO.LOSS_CLAIMS.CLASSIFY_DOCUMENT("FILE_NAME" VARCHAR, "STAGE_NAME" VARCHAR DEFAULT '@INS_CO_DB.ANALYTICS.DOC_STAGE')
-RETURNS OBJECT
-LANGUAGE SQL
-AS '
-    WITH classification_result AS (
-        SELECT AI_EXTRACT(
-            TO_FILE(stage_name, file_name),
-            [
-                ''What type of document is this? Classify as one of: Invoice, Evidence Image, Medical Bill, Insurance Claim, Policy Document, Correspondence, Legal Document, Financial Statement, Other''
-            ]
-        ) as classification_data
-    )
-    SELECT 
-        OBJECT_CONSTRUCT(
-            ''success'', TRUE,
-            ''file_name'', file_name,
-            ''classification_type'', classification_data[0]:answer::STRING,
-            ''description'', classification_data[1]:answer::STRING,
-            ''business_context'', classification_data[2]:answer::STRING,
-            ''document_purpose'', classification_data[3]:answer::STRING,
-            ''confidence_score'', (
-                classification_data[0]:score::NUMBER + 
-                classification_data[1]:score::NUMBER + 
-                classification_data[2]:score::NUMBER + 
-                classification_data[3]:score::NUMBER
-            ) / 4,
-            ''classification_timestamp'', CURRENT_TIMESTAMP(),
-            ''full_classification_data'', classification_data
-        ) as result
-    FROM classification_result
-';
+CREATE OR REPLACE FUNCTION INS_CO.LOSS_CLAIMS.CLASSIFY_DOCUMENT(p_file_name VARCHAR, p_stage_name VARCHAR DEFAULT '@INS_CO.LOSS_CLAIMS.LOSS_EVIDENCE')
+  RETURNS OBJECT
+  LANGUAGE SQL
+  AS
+  $$
+    WITH classification_result_cte
+         AS (SELECT to_file(p_stage_name, p_file_name) AS target_file,
+                    ai_extract(FILE => target_file,
+                               responseFormat =>
+                               ARRAY_CONSTRUCT('What type of document is this? Classify as one of: Invoice, Evidence Image, Medical Bill, Insurance Claim, Policy Document, Correspondence, Legal Document, Financial Statement, Other')
+                    )                                  AS classification_data)
 
-CREATE OR REPLACE FUNCTION INS_CO.LOSS_CLAIMS.PARSE_DOCUMENT_FROM_STAGE("FILE_NAME" VARCHAR)
-RETURNS VARIANT
-LANGUAGE SQL
-AS '
-    SELECT AI_PARSE_DOCUMENT(
-        TO_FILE(''@ins_co.loss_claims.loss_evidence'', file_name),
-        {
-            ''mode'': ''LAYOUT'',
-            ''page_split'': TRUE
-        }
-    )::VARIANT
-';
+    SELECT OBJECT_CONSTRUCT(
+                   'success', TRUE,
+                   'file_name', fl_get_relative_path(classification_result_cte.target_file),
+                   'classification_type', classification_data[0]:answer::STRING,
+                   'description', classification_data[1]:answer::STRING,
+                   'business_context', classification_data[2]:answer::STRING,
+                   'document_purpose', classification_data[3]:answer::STRING,
+                   'confidence_score', (classification_data[0]:score::NUMBER +
+                                        classification_data[1]:score::NUMBER +
+                                        classification_data[2]:score::NUMBER +
+                                        classification_data[3]:score::NUMBER) / 4,
+                   'classification_timestamp', CURRENT_TIMESTAMP(),
+                   'full_classification_data', classification_data
+           ) AS result
+    FROM classification_result_cte
+  $$
+;
+
+CREATE OR REPLACE FUNCTION INS_CO.LOSS_CLAIMS.PARSE_DOCUMENT_FROM_STAGE(p_file_name VARCHAR, p_stage_name VARCHAR DEFAULT '@INS_CO.LOSS_CLAIMS.LOSS_EVIDENCE')
+  RETURNS VARIANT
+  LANGUAGE SQL
+  AS
+  $$
+    WITH ai_parse_doc_cte AS (SELECT to_file(p_stage_name, p_file_name)                                    AS target_file,
+                                     TO_OBJECT(PARSE_JSON('{"mode": "layout", "page_split": true}'))       AS ai_parse_document_options,
+                                     TO_VARIANT(ai_parse_document(target_file, ai_parse_document_options)) AS raw_text_dict)
+
+    SELECT raw_text_dict
+    FROM ai_parse_doc_cte
+  $$
+;
 
 CREATE OR REPLACE FUNCTION INS_CO.LOSS_CLAIMS.GET_IMAGE_SUMMARY("IMAGE_FILE" VARCHAR, "STAGE_NAME" VARCHAR)
 RETURNS VARCHAR
